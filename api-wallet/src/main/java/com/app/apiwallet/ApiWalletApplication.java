1a13b86ac5a712cc303843e1df3e395a24322056
Alej087 <alejo87.gonzalez@gmail.com>
2023-03-06 21:55:32 -0500

    Eureka server, config server and microservices was created

---
+++ api-wallet/src/main/java/com/app/apiwallet/ApiWalletApplication.java
@@ -0,0 +1,13 @@
+package com.app.apiwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWalletApplication.class, args);
+	}
+
+}
